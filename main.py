d72967afdc40ff77e7aabf6893cd14bb7e94152a
R2boyo25 <r2boyo25@gmail.com>
2021-10-24 17:41:36 -0500

    remove gui

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 import sys
-#import GUI
 import CLI
 
-if len(sys.argv) > 1:
-    if sys.argv[1] == '--gui' or sys.argv[1] == '-g':
-        try:
-            import PyQt5
-        except:
-            print('PyQt5 not installed')
-            quit()
-        import GUI
-        GUI.main()
-    else:
-        CLI.main(sys.argv[1:])
-else:
+if len(sys.argv) > 0:
     CLI.main(sys.argv[1:])
+else:
+    print("Please provide at least one argument!")
