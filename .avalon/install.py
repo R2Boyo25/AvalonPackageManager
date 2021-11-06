ada4844c06a0333eea5ef010327a213d46dcfabf
R2boyo25 <r2boyo25@gmail.com>
2021-11-06 14:57:47 -0500

    fix install again

--- .avalon/install.py
+++ .avalon/install.py
@@ -1,14 +1,20 @@
 import os
 import sys
 
-binf = sys.argv[1]
-filesFolder = sys.argv[2]
-binDir = sys.argv[3]
-srcDir = sys.argv[4]
+try:
+    binf = sys.argv[1]
+    filesFolder = sys.argv[2]
+    binDir = sys.argv[3]
+    srcDir = sys.argv[4]
+except:
+    binDir = sys.argv[1]
+    srcDir = sys.argv[2]
+    binf = 'apm'
+    filesFolder = srcDir
 #pkgName = sys.argv[3]
 
 with open(binf, "w") as avalonStarter:
-    filecontent = f'''python3 {fileFolder}/main.py "$@"'''
+    filecontent = f'''python3 {filesFolder}/main.py "$@"'''
     avalonStarter.write(filecontent)
     #os.system("chmod +x " + f"{binDir}/avalon")
 
