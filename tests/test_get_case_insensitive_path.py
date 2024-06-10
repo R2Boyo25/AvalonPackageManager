6946ca875e4a0030ada56ed48b021b270289f6af
Ohin Taylor <kazani@kazani.dev>
2024-06-09 21:20:17 -0500

    tests: Fix the test again

--- tests/test_get_case_insensitive_path.py
+++ tests/test_get_case_insensitive_path.py
@@ -1,7 +1,8 @@
+from pathlib import Path
 from apm.case.case import get_case_insensitive_path
 
 
 def test_get_case_insensitive_path() -> None:
-    assert get_case_insensitive_path("./Apm") == "./apm"
-    assert get_case_insensitive_path("./tests") == "./tests"
-    assert get_case_insensitive_path("./.aVaLoN") == "./.avalon"
+    assert get_case_insensitive_path("./Apm") == Path("apm")
+    assert get_case_insensitive_path("./tests") == Path("tests")
+    assert get_case_insensitive_path("./.aVaLoN") == Path(".avalon")
